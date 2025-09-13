771f99e408c065bfeae3c5df0306c0a0d1217985
Anila-1505 <anilarayapati@gmail.com>
2025-09-13 21:48:26 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
 $host = "localhost";
-$db   = "crud_app";
-$user = "root";
-$pass = "";  // currently empty
+$db   = "crud_app";     // or the actual DB name you created
+$user = "root";         // or other user you created
+$pass = "1234";         // <-- EXACT password you used in step 2
+
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
